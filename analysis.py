bcc22d2bf33c21754199a0467a943a604ed26945
Howon Lee <howon@howonlee.com>
2014-07-28 19:45:58 -0700

    more adjustments made

--- analysis.py
+++ analysis.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 if __name__ == "__main__":
+	if len(sys.argv) != 2:
+		print "put in a text file as argument"
+		sys.exit(1)
 	with open(sys.argv[1], "r") as ex_out:
 		words = ex_out.read()
 		patt = re.compile('([^\s\w]|_)+')
@@ -11,8 +14,8 @@ if __name__ == "__main__":
 		wordlist = words.split()
 		ct = collections.Counter(wordlist)
 		#this is to obtain the log-log property
+		#so it's not actually the log-log graph, but looks exactly like it
 		word_counts = np.array(map(lambda x: math.log(x), map(operator.itemgetter(1), ct.most_common())))
-		#word_counts = np.array(map(operator.itemgetter(1), ct.most_common()))
 		plt.hist(word_counts)
 		plt.yscale('log')
 		plt.show()
